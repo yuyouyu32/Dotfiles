0748e3063cb8cb3752597688648cf76e5fbeede3
yuyouyu <624297803@qq.com>
2022-03-04 21:09:43 +0800

    add: modify Mac zsh files and add install sh files

---
+++ Mac_install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Profile soft link being created..."
+ln -sf ~/TConfig/Dotfiles/Mac_zsh/zshrc ~/.zshrc
+echo "zshrc soft link created!"
+ln -sf ~/TConfig/Dotfiles/Mac_zsh/zprofile ~/.zprofile
+echo "zprofile soft link created!"
+ln -sf ~/TConfig/Dotfiles/git/gitconfig ~/.gitconfig
+echo "gitconfig soft link created!"
+ln -sf ~/TConfig/Dotfiles/p10k/p10k.zsh ~/.p10k.zsh
+echo "p10k.zsh soft link created!"
+ln -sf ~/TConfig/Dotfiles/tmux/tmux.conf ~/.tmux.conf
+echo "tmux.conf soft link created!"
